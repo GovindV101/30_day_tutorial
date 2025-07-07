8bb6f47d2823967a82b7a661d630e0bfc084db96
GOVIND V NAMBOOTHIRI <govindvn111@gmail.com>
2025-07-07 16:13:31 +0530

    Delete AS6_user_registration_system/User_reg_system directory

--- AS6_user_registration_system/User_reg_system/src/com/litmus7/user_reg_system/exception/WeakPasswordException.java
+++
@@ -1,7 +0,0 @@
-package com.litmus7.user_reg_system.exception;
-
-public class WeakPasswordException extends Exception {
-    public WeakPasswordException(String message) {
-        super(message);
-    }
-}
