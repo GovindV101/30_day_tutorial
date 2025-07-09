b020d0778bfc103c283bbb135dd0e6de9b8f2513
GOVIND V NAMBOOTHIRI <govindvn111@gmail.com>
2025-07-09 10:10:21 +0530

    Add files via upload

---
+++ AS6_user_registration_system/User_reg_system/src/com/litmus7/user_reg_system/user_reg.sql
@@ -0,0 +1,19 @@
+
+CREATE DATABASE user_registration;
+USE user_registration;
+
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    age INT NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    
+    CONSTRAINT chk_age CHECK (age >= 18 AND age <= 60),
+    CONSTRAINT chk_email CHECK (email LIKE '%@%.%')
+);
+
+select * from users;
